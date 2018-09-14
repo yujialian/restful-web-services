90d46be2b669eb58a11165f4b43fba30817137f6
Yujia Lian <yujialian@Yujias-MacBook-Pro.local>
2018-09-14 13:18:38 -0400

    Initial Commit

---
+++ src/main/java/com/yujia/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -0,0 +1,12 @@
+package com.yujia.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+}
