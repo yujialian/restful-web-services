692709c428e385eabab013e74fbea245c643225d
Yujia <yujialia@buffalo.edu>
2018-09-15 16:25:27 -0400

    Using spring's responseEntityExceptionHandler to centralize exception handling across the applications.

---
+++ src/main/java/com/yujia/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.yujia.rest.webservices.restfulwebservices.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.yujia.rest.webservices.restfulwebservices.supermarket.CustomerNotFoundException;
+import com.yujia.rest.webservices.restfulwebservices.supermarket.UserNotFoundException;
+
+/*@ControllerAdvice Share exceptions across all the controller.*/
+@ControllerAdvice 
+/*@RestController to indicate that we want to return a JSON error response.*/
+@RestController 
+/*ResponseEntityExceptionHandler abstract class: Provide centralize exception handling.*/
+public class CustomizedResponseEntityExceptionHandler
+extends ResponseEntityExceptionHandler {
+	
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) throws Exception {
+		ExceptionResponse exceptionResponse = 
+				new ExceptionResponse(new Date(), ex.getMessage(), 
+						request.getDescription(false));
+		return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(CustomerNotFoundException.class)
+	public final ResponseEntity<Object> handleUserNotFoundException(CustomerNotFoundException ex, WebRequest request) throws Exception {
+		ExceptionResponse exceptionResponse = 
+				new ExceptionResponse(new Date(), ex.getMessage(), 
+						request.getDescription(false));
+		return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+}
---
+++ src/main/java/com/yujia/rest/webservices/restfulwebservices/exception/ExceptionResponse.java
@@ -0,0 +1,28 @@
+package com.yujia.rest.webservices.restfulwebservices.exception;
+
+import java.util.Date;
+
+public class ExceptionResponse {
+	//Time stamp
+	//message
+	//detail
+	private Date timestamp;
+	private String message;
+	private String details;
+	public ExceptionResponse(Date timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public String getDetails() {
+		return details;
+	}
+	
+}
